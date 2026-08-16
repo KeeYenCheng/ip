7517ad46922f61089c8b2ac31d03ede42512f2f1
YC <yencheng109@gmail.com>
2026-08-17 00:00:59 +0800

    Add greetings and exit

---
+++ src/main/java/Max.java
@@ -0,0 +1,23 @@
+public class Max {
+  private static String banner = "  _____ _____  ___  ___\n" 
+                 + " /     \\\\__  \\ \\  \\/  /\n" 
+                 + "|  Y Y  \\/ __ \\_>    < \n"
+                 + "|__|_|  (____  /__/\\_ \\\n" 
+                 + "      \\/     \\/      \\/\n";
+  private static String greetings = "Sup! I'm Max.\n"
+                     + "What do you need?\n";
+  private static String bars = "________________________________________";
+  private static String bye = "See ya later!\n";
+
+
+  public static void main(String[] args) {
+
+    System.out.println(banner);
+    System.out.println(greetings);
+    System.out.println(bars);
+    System.out.println(bye);
+    System.out.println(bars);
+
+
+    }
+}
