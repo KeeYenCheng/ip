b253e8ab1fb0f25c8e4c5686a810bdfcf6f465e1
YC <yencheng109@gmail.com>
2026-08-17 22:11:13 +0800

    Create greeting and echo function

--- src/main/java/Max.java
+++ src/main/java/Max.java
@@ -1,23 +1,51 @@
+import java.util.Scanner;
+
 public class Max {
+  private static String tabSpace = "    ";
   private static String banner = "  _____ _____  ___  ___\n" 
                  + " /     \\\\__  \\ \\  \\/  /\n" 
                  + "|  Y Y  \\/ __ \\_>    < \n"
                  + "|__|_|  (____  /__/\\_ \\\n" 
                  + "      \\/     \\/      \\/\n";
-  private static String greetings = "Sup! I'm Max.\n"
-                     + "What do you need?\n";
-  private static String bars = "________________________________________";
-  private static String bye = "See ya later!\n";
+  private static String greetings = tabSpace + "Sup! I'm Max.\n"
+                     + tabSpace + "What do you need?";
+  private static String bars = tabSpace + "________________________________________";
+  private static String bye = tabSpace + "See ya later!\n";
+  
 
-
-  public static void main(String[] args) {
-
+  public static void greeting() {
     System.out.println(banner);
     System.out.println(greetings);
     System.out.println(bars);
-    System.out.println(bye);
-    System.out.println(bars);
+  }
+
+  public static boolean echo(String response) {
+     
+    switch (response) {
+      case "bye":
+        System.out.println(bye);
+        return true;
+      default:
+        System.out.println(tabSpace + response);
+        System.out.println(bars);
+        return false;
+      }
+    
+  }
 
 
+
+  public static void main(String[] args) {
+    
+    greeting();
+    boolean exit = false;
+   
+    while(!exit) {
+      Scanner scanner = new Scanner(System.in);
+      String response = scanner.nextLine();
+      System.out.println(bars); 
+    
+      exit = echo(response); 
     }
+  }
 }
